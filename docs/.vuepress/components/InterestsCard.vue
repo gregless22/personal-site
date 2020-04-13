3796215d59b6139dc613a98c80d5f0838d05ce5f
gregless22 <gregoryconnolly@gmail.com>
2020-04-13 16:23:43 +1000

    slots

--- docs/.vuepress/components/InterestsCard.vue
+++ docs/.vuepress/components/InterestsCard.vue
@@ -1,9 +1,7 @@
 <template>
 <div>
-    <Content slot-key="icon"/>
-    <Content slot-key="description"/>
-    <!-- <slot name="icon" class="icon"></slot>
-    <slot name="description" class="description"></slot> -->
+    <slot name="icon" class="icon"></slot>
+    <slot name="description" class="description"></slot>
 </div>
 </template>
 
