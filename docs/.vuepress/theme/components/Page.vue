4c3239b1f50999894340fa771f958c0899656617
gregless22 <gregoryconnolly@gmail.com>
2020-04-19 10:33:24 +1000

    cleaned up page.vue

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -3,30 +3,18 @@
     <slot name="top" />
 
     <Content class="theme-default-content" />
-    <div v-for="page in pages">hello</div>
-    <PageEdit />
-  {{pages}}
-    <PageNav v-bind="{ sidebarItems }" />
+
+
 
     <slot name="bottom" />
   </main>
 </template>
 
 <script>
-import PageEdit from "@theme/components/PageEdit.vue"
-import PageNav from "@theme/components/PageNav.vue"
 
 export default {
-  components: { PageEdit, PageNav },
+
   props: ["sidebarItems"],
-  data() {
-    return {
-      pages: []
-    }
-  },
-  mounted() {
-    pages = this.$site.pages
-  }
 }
 </script>
 
